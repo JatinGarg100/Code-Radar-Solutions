560896d1f2eb29c6b9f705bea694ab2ff30f9d06
JatinGarg100 <jatin.garg00584@gmail.com>
2025-03-30 16:48:29 +0530

    Solution to Count Elements Greater Than a Given Value

--- Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
+++ Count Elements Greater Than a Given Value@@676c02002dbc8c3e7a280b8a/code.c
@@ -5,7 +5,7 @@ int countGreater(int arr[],int size,int value){
     for(int i=0;i<size;i++){
         if(arr[i]>value){
             count++;
-            return count;
+            
         }
     }
     return count;
